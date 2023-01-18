ed97703ed87fb4ae35a53eff9d16e26596e086aa
roamingterra <danielballerini1@gmail.com>
2023-01-18 10:31:25 -0500

    Generate all books found in library
    My displayBooks function now has a for loop and was modified in such a
    way to now display all books found in the myLibrary array, onto the UI.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+// let myLibraryProperties = [];
 
 function Book(title, author, numOfPages, read) {
   // the constructor...
@@ -42,6 +43,16 @@ function addBookToLibrary() {
   // Store new book object into array
   myLibrary.push(object2);
 
+  // Populate library with another book for the time being
+  let object3 = new Book();
+  object3.title = "Lord of the Rings";
+  object3.author = "J.R. Tolkien";
+  object3.numOfPages = 800;
+  object3.read = "Yes";
+
+  // Store new book object into array
+  myLibrary.push(object3);
+
   // THIS IS ALL WRONG, BECAUSE AFTER THE OBJECT IS STORED IN THE ARRAY,
   // I AM STILL MODIFYING THE OBJECT AND MAKING ANOTHER COPY OF THE SAME
   // OBJECT IN THE ARRAY. I NEED TO CREATE A NEW OBJECT, FILL IT WITH NEW BOOK INFO,
@@ -53,43 +64,46 @@ function addBookToLibrary() {
 function displayBooks() {
   let card = document.querySelector(".cards");
 
-  // Create elements
-  let cardContainer = document.createElement("div");
-  cardContainer.classList.add("card-container");
-  let iconContainer = document.createElement("div");
-  iconContainer.classList.add("icon");
-  let icon = document.createTextNode("x");
-  let title = document.createElement("div");
-  title.classList.add("title");
-  let titleText = document.createTextNode("Title");
-  let pages = document.createElement("div");
-  let pagesText = document.createTextNode("Pages");
-  let author = document.createElement("div");
-  let authorText = document.createTextNode("Author");
+  for (let i = 0; i < myLibrary.length; i++) {
+    // Create elements
+    let cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+    let iconContainer = document.createElement("div");
+    iconContainer.classList.add("icon");
+    let icon = document.createTextNode("x");
+    let title = document.createElement("div");
+    title.classList.add("title");
+    let titleText = document.createTextNode(myLibrary[i].title);
+    let pages = document.createElement("div");
+    let pagesText = document.createTextNode(myLibrary[i].numOfPages);
+    let author = document.createElement("div");
+    let authorText = document.createTextNode(myLibrary[i].author);
 
-  // Append elements
-  card.appendChild(cardContainer);
-  cardContainer.appendChild(iconContainer);
-  iconContainer.appendChild(icon);
-  cardContainer.appendChild(title);
-  title.appendChild(titleText);
-  cardContainer.appendChild(pages);
-  pages.appendChild(pagesText);
-  cardContainer.appendChild(author);
-  author.appendChild(authorText);
+    // Append elements
+    card.appendChild(cardContainer);
+    cardContainer.appendChild(iconContainer);
+    iconContainer.appendChild(icon);
+    cardContainer.appendChild(title);
+    title.appendChild(titleText);
+    cardContainer.appendChild(pages);
+    pages.appendChild(pagesText);
+    cardContainer.appendChild(author);
+    author.appendChild(authorText);
 
-  // Style elements
-  cardContainer.style.display = "flex";
-  cardContainer.style.flexDirection = "column";
-  cardContainer.style.alignItems = "center";
-  cardContainer.style.padding = "5px 20px 20px 20px";
-  cardContainer.style.color = "white";
-  cardContainer.style.backgroundColor = "#171717";
-  iconContainer.style.alignSelf = "flex-end";
-  iconContainer.style.fontSize = "1.5rem";
-  title.style.fontSize = "3rem";
-  pages.style.padding = "40px 0";
-  author.style.fontSize = "1.5rem";
+    // Style elements
+    cardContainer.style.display = "flex";
+    cardContainer.style.flexDirection = "column";
+    cardContainer.style.alignItems = "center";
+    cardContainer.style.padding = "5px 20px 20px 20px";
+    cardContainer.style.color = "white";
+    cardContainer.style.backgroundColor = "#171717";
+    iconContainer.style.alignSelf = "flex-end";
+    iconContainer.style.fontSize = "1.5rem";
+    title.style.fontSize = "3rem";
+    title.style.textAlign = "center";
+    pages.style.padding = "40px 0";
+    author.style.fontSize = "1.5rem";
+  }
 }
 
 addBookToLibrary();
