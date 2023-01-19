e7a08288a27b4fec94f3b6a53c9216a6d9879c2f
roamingterra <danielballerini1@gmail.com>
2023-01-19 16:38:50 -0500

    Make Modal Form appear and disappear
    The Modal Form now appears with the click of the "Add Book" button, and
    disappears when the x icon is clicked, or when a click is made outside
    of the Modal Form. I originally tried to create and style the form
    through the JavaScript, however I found that I could leave the form in
    the html, leave the styling in the CSS, and simply use event listeners
    in JavaScript to show and hide the form. I left up the commented out
    JavaScript code that creates and stylizes the Modal Form for this commit
    only just to show my work.

--- script.js
+++ script.js
@@ -70,11 +70,222 @@ function addBookToLibrary() {
 }
 
 function addBook() {
-  let button = document.querySelector("button");
+  // Get the body
+  let body = document.querySelector("body");
+
+  // Get the modal window
+  let formContainer = document.querySelector(".form-container");
+
+  // Get the button that opens the modal window
+  let buttonAddBook = document.querySelector(".add-book");
+
+  // Get the element that closes the modal window
+  let cancel = document.querySelector(".cancel");
+
+  // When the user clicks on the button
+  buttonAddBook.addEventListener("click", () => {
+    formContainer.style.display = "flex";
+  });
+
+  // When the user clicks on <div class="cancel"> (x), close the modal window
+  cancel.addEventListener("click", () => {
+    formContainer.style.display = "none";
+  });
+
+  //   When the user clicks anywhere outside of the modal, close it
+  document.addEventListener("click", (event) => {
+    if (
+      !event.target.closest(".form-container") && // The closest method looks for the parent .form-container when anything inside is clicked
+      !event.target.matches(".add-book")
+    ) {
+      formContainer.style.display = "none";
+      console.log(event.target);
+    }
+  });
+
+  /*
+  let button = document.querySelector(".add-book");
   button.addEventListener("click", () => {
+    let body = document.querySelector("body");
+
     // Create elements
     let formContainer = document.createElement("div");
+    formContainer.classList.add("form-container");
+
+    let cancel = document.createElement("div");
+    cancel.classList.add("cancel");
+    let cancelText = document.createTextNode("x");
+
+    let form = document.createElement("form");
+    form.setAttribute("action", "script.js");
+    form.setAttribute("method", "post");
+    form.setAttribute("id", "form");
+
+    let formTitle = document.createElement("div");
+    formTitle.classList.add("form-title");
+    let titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "title");
+    let titleText = document.createTextNode("TITLE");
+    let titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("id", "title");
+    titleInput.required = true;
+    titleInput.setAttribute("maxlength", "30");
+
+    let formAuthor = document.createElement("div");
+    formAuthor.classList.add("form-author");
+    let authorLabel = document.createElement("label");
+    authorLabel.setAttribute("for", "author");
+    let authorText = document.createTextNode("AUTHOR");
+    let authorInput = document.createElement("input");
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("id", "author");
+    authorInput.required = true;
+    authorInput.setAttribute("maxlength", "15");
+
+    let formNumberOfPages = document.createElement("div");
+    formNumberOfPages.classList.add("form-number-of-pages");
+    let numberOfPagesLabel = document.createElement("label");
+    numberOfPagesLabel.setAttribute("for", "number-of-pages");
+    let numberOfPagesText = document.createTextNode("NUMBER OF PAGES");
+    let numberOfPagesInput = document.createElement("input");
+    numberOfPagesInput.setAttribute("type", "number");
+    numberOfPagesInput.setAttribute("id", "number-of-pages");
+    numberOfPagesInput.required = true;
+    numberOfPagesInput.setAttribute("min", "1");
+    numberOfPagesInput.setAttribute("max", "10000");
+
+    let formRead = document.createElement("div");
+    formRead.classList.add("form-read");
+    let readLabel = document.createElement("label");
+    readLabel.setAttribute("for", "read");
+    let readText = document.createTextNode("WAS THIS BOOK READ?");
+    let checkBoxContainer = document.createElement("div");
+    checkBoxContainer.classList.add("check-box-container");
+    let yes = document.createElement("div");
+    let yesText = document.createTextNode("YES");
+    let readInput = document.createElement("input");
+    readInput.setAttribute("type", "checkbox");
+    readInput.setAttribute("name", "decision");
+    readInput.setAttribute("id", "read");
+    readInput.setAttribute("value", "YES");
+
+    let submitButton = document.createElement("button");
+    submitButton.setAttribute("type", "submit");
+    submitButton.setAttribute("id", "submit");
+    submitButton.setAttribute("form", "form");
+    submitButton.setAttribute("onclick", "addBook()");
+    let submitButtonText = document.createTextNode("SUBMIT");
+
+    // Append elements
+    body.appendChild(formContainer);
+    formContainer.appendChild(cancel);
+    cancel.appendChild(cancelText);
+
+    formContainer.appendChild(form);
+
+    form.appendChild(formTitle);
+
+    formTitle.appendChild(titleLabel);
+    titleLabel.appendChild(titleText);
+    formTitle.appendChild(titleInput);
+
+    form.appendChild(formAuthor);
+
+    formAuthor.appendChild(authorLabel);
+    authorLabel.appendChild(authorText);
+    formAuthor.appendChild(authorInput);
+
+    form.appendChild(formNumberOfPages);
+
+    formNumberOfPages.appendChild(numberOfPagesLabel);
+    numberOfPagesLabel.appendChild(numberOfPagesText);
+    formNumberOfPages.appendChild(numberOfPagesInput);
+
+    form.appendChild(formRead);
+
+    formRead.appendChild(readLabel);
+    readLabel.appendChild(readText);
+    formRead.appendChild(checkBoxContainer);
+    checkBoxContainer.appendChild(yes);
+    yes.appendChild(yesText);
+    checkBoxContainer.appendChild(readInput);
+
+    form.appendChild(submitButton);
+    submitButton.appendChild(submitButtonText);
+
+    // Style elements
+    cancel.style.position = "absolute";
+    cancel.style.left = "385px";
+    cancel.style.top = "5px";
+    cancel.style.fontSize = "2rem";
+    cancel.style.cursor = "pointer";
+
+    formContainer.style.display = "flex";
+    formContainer.style.justifyContent = "center";
+    formContainer.style.alignItems = "center";
+    formContainer.style.position = "absolute";
+    formContainer.style.top = "54vh";
+    formContainer.style.left = "50vw";
+    formContainer.style.marginTop = "-150px";
+    formContainer.style.marginLeft = "-210px";
+    formContainer.style.width = "420px";
+    formContainer.style.height = "300px";
+    formContainer.style.zIndex = "100";
+    formContainer.style.borderRadius = "20px";
+    formContainer.style.backgroundColor = "#ff6c00";
+
+    form.style.display = "flex";
+    form.style.flexDirection = "column";
+    form.style.alignItems = "center";
+    form.style.justifyContent = "center";
+    form.style.gap = "10px";
+
+    formChildren = document.querySelector("form").children;
+
+    for (let i = 0; i < formChildren.length; i++) {
+      formChildren[i].style.display = "flex";
+      formChildren[i].style.flexDirection = "column";
+    }
+
+    numberOfPagesInput.style.width = "139px";
+
+    formRead.style.gap = "5px";
+
+    checkBoxContainer.style.display = "flex";
+    checkBoxContainer.style.justifyContent = "center";
+
+    readInput.style.cursor = "pointer";
+
+    submitButton.style.borderRadius = "50px";
+    submitButton.style.padding = "10px";
+    submitButton.style.fontFamily = "RobotoCondensedBold";
+    submitButton.style.fontSize = "1rem";
+    submitButton.style.backgroundColor = "white";
+    submitButton.style.color = "#171717";
+    submitButton.style.border = "0";
+
+    submitButton.addEventListener("mouseenter", () => {
+      submitButton.style.backgroundColor = "#171717";
+      submitButton.style.color = "white";
+    });
+
+    submitButton.addEventListener("mouseleave", () => {
+      submitButton.style.backgroundColor = "white";
+      submitButton.style.color = "#171717";
+    });
+
+    submitButton.addEventListener("mousedown", () => {
+      submitButton.style.backgroundColor = "white";
+      submitButton.style.color = "#171717";
+    });
+
+    submitButton.addEventListener("mouseup", () => {
+      submitButton.style.backgroundColor = "#171717";
+      submitButton.style.color = "white";
+    });
   });
+  */
 }
 
 function displayBooks() {
@@ -136,3 +347,5 @@ function displayBooks() {
 addBookToLibrary();
 
 displayBooks();
+
+addBook();
